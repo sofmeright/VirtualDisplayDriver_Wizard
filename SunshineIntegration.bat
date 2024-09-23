b9e3492cda7c4b2b9faa8f2f6cec02672e6bfda0
sofmeright <kai.hamil@gmail.com>
2024-09-23 22:30:04 +0000

    Pushing the change necessary to close Issue #4 ~ SunshineIntegration.bat ~ not determining the Wizard's directory reliably. modified:   SunshineIntegration.bat

--- SunshineIntegration.bat
+++ SunshineIntegration.bat
@@ -13,7 +13,7 @@ set HEIGHT=%2
 set FPS=%3
 
 :: Run the commands with the provided arguments
-start "" /w PRPlanIT.com-VirtualDisplayDrv_Wiz.exe Reso_Adds %WIDTH% %HEIGHT% %FPS% && start "" /w PRPlanIT.com-VirtualDisplayDrv_Wiz.exe Driv_Relo
+start "" /w "%~dp0PRPlanIT.com-VirtualDisplayDrv_Wiz.exe" Reso_Adds %WIDTH% %HEIGHT% %FPS% && start "" /w "%~dp0PRPlanIT.com-VirtualDisplayDrv_Wiz.exe" Driv_Relo
 
 :: Optionally pipe the output to a log file (if needed)
 PRPlanIT.com-VirtualDisplayDrv_Wiz.exe Reso_Adds %WIDTH% %HEIGHT% %FPS% > output.log 2>&1
